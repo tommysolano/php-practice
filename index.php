544ac2a7e36665b01bade92bf55452cbbb1c8162
tommy <tommysolano18@hotmail.com>
2021-10-31 01:26:06 -0500

    sintaxis basica php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Comentario de varias
+ * lineas para
+ * palabra
+ */
+
+ class Answer 
+ {
+    /**
+    * Comentario de varias
+    * lineas para
+    * palabra
+    */
+    protected $clients = []; //comentario
+
+    /**
+    * Comentario de varias
+    * lineas para
+    * palabra
+    */
+    protected $insurers = []; //comentario
+ }
