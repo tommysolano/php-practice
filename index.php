a93b3d825ab688a0ab44f6e398099794269acd96
tommy <tommysolano18@hotmail.com>
2021-10-31 01:35:29 -0500

    asignacion y aritmetica

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 
-/**
- * Comentario de varias
- * lineas para
- * palabra
- */
+//Asignacion
+$num = 9;
+$lang = [
+    "es" => "español",
+    "en" => "ingles"
+];
 
- class Answer 
- {
-    /**
-    * Comentario de varias
-    * lineas para
-    * palabra
-    */
-    protected $clients = []; //comentario
+//Aritmetica
+echo "Suma 2 + 2 " . ((int) 2 + (int) 2 );
+echo "Resta 2 - 2 " . ((int) 2 - (int) 2 );
+echo "Multiplica 2 * 2 " . 2 * 2 ;
+echo "Divide 2 / 2 " . 2 / 2 ;
+echo "Modulo 2 % 2 " . 2 % 2 ;
+echo "Exponencial 2 ** 2 " . 2 ** 2 ;
 
-    /**
-    * Comentario de varias
-    * lineas para
-    * palabra
-    */
-    protected $insurers = []; //comentario
- }
+
+
